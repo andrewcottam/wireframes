e06d53af07e32ca427332f2be3f0676de0a1588c
Andrew Cottam <a.cottam@gmail.com>
2018-02-06 14:51:06 +0100

    added indicator card

--- regional/src/App.js
+++ regional/src/App.js
@@ -11,6 +11,7 @@ import FlatButton from 'material-ui/FlatButton';
 import Dialog from 'material-ui/Dialog';
 import { List, ListItem } from 'material-ui/List';
 import { Step, Stepper, StepLabel } from 'material-ui/Stepper';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import Subheader from 'material-ui/Subheader';
 // import Badge from 'material-ui/Badge';
 import Paper from 'material-ui/Paper';
@@ -21,50 +22,85 @@ import logo_r1 from './logo-r1.png';
 import logo_fji from './logo-fji.png';
 import logo_slb from './logo-slb.png';
 import logo_l1 from './logo-l1.png';
+import intactForest from './intactForest.png';
 
 class PoliciesDrawer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { open: true };
+    this.state = { open: true, indicatorOpen: false, activeIndicator: null };
   }
 
   buttonClick() {
     this.setState({ open: !this.state.open });
   }
 
-  close() {
-    this.setState({ open: false });
+  showIndicator(e) {
+    this.setState({ open: false, indicatorOpen: true, activeIndicator: e });
+  }
+
+  hideIndicator(e) {
+    // this.setState({ indicatorOpen: false });
   }
 
   render() {
-    return <Drawer open={this.state.open} onRequestChange={(open) => this.setState({open})} containerStyle={{'position': 'absolute', 'top': '64px','overflow':'none'}} width={360}>
+    return <React.Fragment>
+    <Drawer open={this.state.open} onRequestChange={(open) => this.setState({open})} containerStyle={{'position': 'absolute', 'top': '64px','overflow':'none'}} width={360}>
     <RaisedButton label="Knowledge" secondary={true} style={{"transform":"rotate(270deg)",'left': '319px','top': '360px','position': 'absolute'}} onClick={this.buttonClick.bind(this)}/>
-    <PoliciesPanel onIndicatorSelected={this.close.bind(this)}/>
+    <PoliciesPanel onIndicatorSelected={this.showIndicator.bind(this)}/>
     </Drawer>;
+    <IndicatorChart isOpen={this.state.indicatorOpen} indicator={this.state.activeIndicator}/>
+    </React.Fragment>;
   }
 }
 
-class PoliciesPanel extends React.Component{
-  indicatorSelected(e){
+class IndicatorChart extends React.Component {
+  render() {
+    let s = { 'position': 'absolute', left: '45px', 'width': '400px' };
+    let containerStyle = {'boxShadow': "rgba(0, 0, 0, 0.25) 0px 14px 45px, rgba(0, 0, 0, 0.22) 0px 10px 18px"};
+    return (this.props.isOpen ?
+      <Card style={s} containerStyle={containerStyle}>
+          <CardHeader
+            title={this.props.indicator.policyListItem.props.primaryText}
+            subtitle={this.props.indicator.policyListItem.props.secondaryText}
+            avatar={this.props.indicator.policyListItem.props.leftAvatarSrc}
+          />
+          <CardMedia
+            overlay={<CardTitle title={this.props.indicator.targetListItem.props.primaryText} subtitle={this.props.indicator.targetListItem.props.secondaryText.length>55 ? this.props.indicator.targetListItem.props.secondaryText.substring(0,55) + ".." : this.props.indicator.targetListItem.props.secondaryText}/>}
+            title={this.props.indicator.targetListItem.props.secondaryText.length>55 ? this.props.indicator.targetListItem.props.secondaryText : null}
+          >
+            <img src={intactForest} alt="" />
+          </CardMedia>
+          <CardTitle subtitle={this.props.indicator.indicatorListItem.props.secondaryText}/>
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat vol
+          </CardText>
+        </Card> : null
+    );
+  }
+}
+
+class PoliciesPanel extends React.Component {
+  indicatorSelected(e) {
     this.props.onIndicatorSelected(e);
   }
-  render(){
-  return <div><List>
+  render() {
+    return <div><List>
     <Subheader>Policies</Subheader>
     <PolicyGroupHeader primaryText="Global" nestedItems={[
       <PolicyListItem key="g1" primaryText="Convention on Biological Diversity" secondaryText="Strategic Plan for Biodiversity 2011-2020" leftAvatarSrc={logo_g1} onIndicatorSelected={this.indicatorSelected.bind(this)} targets={[
         <div disabled={true} key="t1" primaryText="CBD Target 1" secondaryText="By 2020, at the latest, people are aware of the values of biodiversity and the steps they can take to conserve and use it sustainably."/>,
         <div disabled={true} key="t2" primaryText="CBD Target 2" secondaryText="By 2020, at the latest, biodiversity values have been integrated into national and local development and poverty reduction strategies and planning processes and are being incorporated into national accounting, as appropriate, and reporting systems."/>,
         <div key="t11" primaryText="CBD Target 11" secondaryText="By 2020, at least 17 per cent of terrestrial and inland water areas and 10 per cent of coastal and marine areas, especially areas of particular importance for biodiversity and ecosystem services, are conserved through effectively and equitably managed, ecologically representative and well-connected systems of protected areas and other effective area-based conservation measures, and integrated into the wider landscape and seascape." indicators={[
-          <div key="i1" primaryText="Terrestrial protected area coverage" secondaryText="The total non-overlapping area of terrestrial protected areas" chartTitle="Number of countries"/>,
+          <div key="i1" primaryText="Terrestrial protected area coverage" secondaryText="The total non-overlapping area of terrestrial protected areas"/>,
           <div key="i2" primaryText="Marine protected area coverage" secondaryText="The total non-overlapping area of marine protected areas"/>
           ]}/>
       ]}/>,
-      <PolicyListItem key="g2" primaryText="Sustainable Development Goals" secondaryText="This is a fantastic groovy thing" leftAvatarSrc={logo_g2}/>
+      <PolicyListItem key="g2" primaryText="Sustainable Development Goals" secondaryText="This is a fantastic groovy thing" leftAvatarSrc={logo_g2} onIndicatorSelected={this.indicatorSelected.bind(this)}/>
     ]}
     />
     <PolicyGroupHeader primaryText="Regional" nestedItems={[
-      <PolicyListItem key="r1" primaryText="Framework for Nature Conservation and Protected Areas in the Pacific Islands Region" secondaryText="2014 - 2020" leftAvatarSrc={logo_r1} targets={[
+      <PolicyListItem key="r1" primaryText="Framework for Nature Conservation and Protected Areas in the Pacific Islands Region" secondaryText="2014 - 2020" leftAvatarSrc={logo_r1} onIndicatorSelected={this.indicatorSelected.bind(this)} targets={[
         <div disabled={true} key="t1" primaryText="Objective 1" secondaryText="People are aware of the value of biodiversity and the steps they can take to use it sustainably"/>,
         <div disabled={true} key="t2" primaryText="Objective 2" secondaryText="Both economic development and biodiversity conservation recognise and support sustainable livelihoods, cultural heritage, knowledge and expressions, and community resilience and development aspirations"/>,
         <div key="t2" primaryText="Objective 3" secondaryText="Identify, conserve, sustainably manage and restore priority sites, habitats and ecosystems, including cultural sites" indicators={[
@@ -75,23 +111,24 @@ class PoliciesPanel extends React.Component{
     ]}
     />
     <PolicyGroupHeader primaryText="National" nestedItems={[
-      <PolicyListItem key="n1" primaryText="Fiji National Biodiversity Strategy and Action Plan" secondaryText="2017-2030" leftAvatarSrc={logo_fji}/>,
-      <PolicyListItem key="n2" primaryText="Solomon Islands National Biodiversity Strategy and Action Plan" secondaryText="This is a fantastic groovy thing" leftAvatarSrc={logo_slb}/>
+      <PolicyListItem key="n1" primaryText="Fiji National Biodiversity Strategy and Action Plan" secondaryText="2017-2030" leftAvatarSrc={logo_fji} onIndicatorSelected={this.indicatorSelected.bind(this)}/>,
+      <PolicyListItem key="n2" primaryText="Solomon Islands National Biodiversity Strategy and Action Plan" secondaryText="This is a fantastic groovy thing" leftAvatarSrc={logo_slb} onIndicatorSelected={this.indicatorSelected.bind(this)}/>
     ]}
     />
     <PolicyGroupHeader primaryText="Local" nestedItems={[
-      <PolicyListItem key="l1" primaryText="O Le Pupū Puē National Park" secondaryText="Management Plan" leftAvatarSrc={logo_l1} targets={[
+      <PolicyListItem key="l1" primaryText="O Le Pupū Puē National Park" secondaryText="Management Plan" leftAvatarSrc={logo_l1} onIndicatorSelected={this.indicatorSelected.bind(this)} targets={[
         <div key="t1" primaryText="Objective 1" secondaryText="Increase management effectiveness" indicators={[
           <div key="i3" primaryText="IMET Species Indicator" secondaryText="The total number of bunnies"/>,
           <div key="i4" primaryText="IMET Threat Assessment" secondaryText="The level of bad things happening"/>
           ]}/>
       ]}/>,
-      <PolicyListItem key="l2" primaryText="Ngiri-Tumba-Maindombe NP" secondaryText="Management Plan" leftAvatarSrc={logo_l1}/>
+      <PolicyListItem key="l2" primaryText="Ngiri-Tumba-Maindombe NP" secondaryText="Management Plan" leftAvatarSrc={logo_l1} onIndicatorSelected={this.indicatorSelected.bind(this)}/>
     ]}
     />
   </List>
   <PanelLowerToolbar/>
-  </div>}
+  </div>
+  }
 }
 
 function PolicyGroupHeader(props) {
@@ -110,7 +147,8 @@ class PolicyListItem extends React.Component {
 
   indicatorSelected(e) {
     this.setState({ targetsOpen: false });
-    this.props.onIndicatorSelected(e);
+    let hierarchy = Object.assign({}, e.hierarchy, { policyListItem: this });
+    this.props.onIndicatorSelected(hierarchy);
   }
 
   handleClick() {
@@ -155,7 +193,7 @@ class TargetListItem extends React.Component {
 
   indicatorSelected(e) {
     this.setState({ indicatorsOpen: false });
-    this.props.indicatorSelected(e);
+    this.props.indicatorSelected({ hierarchy: { indicatorListItem: e, targetListItem: this } });
   }
 
   handleClick() {
@@ -199,17 +237,8 @@ class IndicatorsDrawer extends React.Component {
 }
 
 class IndicatorListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { open: false };
-  }
   indicatorSelected(e) {
-    this.props.onClick(e);
-    this.setState({ open: true });
-  }
-  handleClose(e){
-    this.setState({ open: false });
-    
+    this.props.onClick(this);
   }
   render() {
     return (<React.Fragment>
@@ -220,12 +249,6 @@ class IndicatorListItem extends React.Component {
       onClick={this.indicatorSelected.bind(this)}
     >
     </ListItem>
-    <Dialog title={this.props.chartTitle}
-      open={this.state.open}
-      overlayStyle={{backgroundColor: 'transparent'}}
-      onRequestClose={this.handleClose.bind(this)}
-      modal={false}
-    />
     </React.Fragment>);
   }
 }
