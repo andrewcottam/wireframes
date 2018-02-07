5314e2223bfcf2b7e9fa819a43d9a4c348305bb8
Andrew Cottam <a.cottam@gmail.com>
2018-02-07 17:23:08 +0100

    drilling down works

--- regional/src/App.js
+++ regional/src/App.js
@@ -71,14 +71,27 @@ function getCountries(map) {
   return countries;
 }
 
-class CountryListItem extends React.Component{
-  drillCountry(){
-    this.props.drillCountry({countryListItem:this});
+class CountryListItem extends React.Component {
+  drillCountry() {
+    this.props.drillCountry({ countryListItem: this });
   }
-  render(){
+  render() {
     return (
       <React.Fragment>
-        <ListItem primaryText={this.props.primaryText} leftAvatar={this.props.leftAvatar} onClick={this.drillCountry.bind(this)}/> 
+        <ListItem primaryText={this.props.primaryText} leftAvatar={this.props.leftAvatar} onClick={this.drillCountry.bind(this)} innerDivStyle={{padding:'16px 16px 16px 72px'}}/> 
+      </React.Fragment>
+    );
+  }
+}
+
+class ProvinceListItem extends React.Component {
+  drillProvince() {
+    this.props.drillProvince({ provinceListItem: this });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <ListItem primaryText={this.props.primaryText} onClick={this.drillProvince.bind(this)} innerDivStyle={{padding:'2px 10px 2px 10px'}}/> 
       </React.Fragment>
     );
   }
@@ -87,7 +100,7 @@ class CountryListItem extends React.Component{
 class IndicatorChart extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: 'summary',selectedCountry: null };
+    this.state = { value: 'summary', selectedCountry: null, selectedProvince: null };
   }
   handleChange = (value) => {
     this.setState({
@@ -98,17 +111,14 @@ class IndicatorChart extends React.Component {
     this.setState({ value: 'countries' });
     this.props.map.setCenter([161.76, -8.14]);
     this.props.map.zoomTo(4);
-    this.props.map.on("moveend", function(e) {
-      // console.log("moveend");
-      // let countries = getCountries(this);
-    });
-    this.props.map.on("render", function(e) {
-      // console.log("render");
-      // let countries = getCountries(this);
-    });
   }
   drillCountry(e) {
     this.setState({ value: 'country', selectedCountry: e.countryListItem });
+    this.props.map.fitBounds(e.countryListItem.props.bounds);
+  }
+  drillProvince(e) {
+    this.setState({ value: 'province', selectedProvince: e.provinceListItem });
+    this.props.map.fitBounds(e.provinceListItem.props.bounds);
   }
   render() {
     let s = { position: 'absolute', left: '45px', width: '440px' };
@@ -146,23 +156,27 @@ class IndicatorChart extends React.Component {
             <List>
               <CountryListItem 
                 primaryText="Papua New Guinea" 
-                leftAvatar={<Avatar src={logo_png} />}
+                leftAvatar={<Avatar src={logo_png} size={30} style={{top:"9px"}}/>}
                 drillCountry={this.drillCountry.bind(this)}
+                bounds={[140.8343505859375,-11.655380249023438,157.03778076171886,-0.7558330297469293]}
               />
               <CountryListItem
                 primaryText="Solomon Islands"
-                leftAvatar={<Avatar src={logo_slb} />}
+                leftAvatar={<Avatar src={logo_slb} size={30} style={{top:"9px"}}/>}
                 drillCountry={this.drillCountry.bind(this)}
+                bounds={[155.3925018310548,-12.308334350585824,170.19250488281273,-4.44521999359124]}
               />
               <CountryListItem
                 primaryText="Timor Leste"
-                leftAvatar={<Avatar src={logo_tls} />}
+                leftAvatar={<Avatar src={logo_tls} size={30} style={{top:"9px"}}/>}
                 drillCountry={this.drillCountry.bind(this)}
+                bounds={[124.04465484619152,-9.50465297698969,127.34249877929703,-8.126944541931156]}
               />
               <CountryListItem
                 primaryText="Vanuatu"
-                leftAvatar={<Avatar src={logo_vut}/>}
+                leftAvatar={<Avatar src={logo_vut} size={30} style={{top:"9px"}}/>}
                 drillCountry={this.drillCountry.bind(this)}
+                bounds={[166.54139709472656,-22.400554656982422,172.09008789062503,-13.072476387023903]}
               />
             </List>
           </Tab>
@@ -173,16 +187,44 @@ class IndicatorChart extends React.Component {
               {this.state.selectedCountry ? 
                 <React.Fragment>
                   <List>
-                    <ListItem primaryText={this.state.selectedCountry.props.primaryText} leftAvatar={this.state.selectedCountry.props.leftAvatar} />
+                    <ListItem primaryText={this.state.selectedCountry.props.primaryText} leftAvatar={this.state.selectedCountry.props.leftAvatar} innerDivStyle={{padding:'16px 16px 16px 72px'}}/>
                   </List> 
                   <Divider />
+                  <List>
+                    <ProvinceListItem primaryText="Central" bounds={[146.38201904296886,-10.409167289733773,149.66452026367213,-7.7725348472595215]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Chimbu" bounds={[144.42181396484386,-6.868734836578312,145.35041809082034,-5.775935173034526]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="East New Britain" bounds={[150.594207763672,-6.07494401931757,152.49851989746108,-4.087428092956465]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="East Sepik" bounds={[141.33921813964844,-5.158234119415283,144.83367919921875,-3.1891660690306907]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Eastern Highlands" bounds={[144.98760986328125,-7.158435821533203,146.13621520996094,-5.852434158325202]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Enga" bounds={[142.74942016601585,-5.975834846496582,144.24871826171875,-5.010334014892511]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Gulf" bounds={[142.994110107422,-8.587371826171818,146.65251159667991,-6.7054347991942755]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Madang" bounds={[143.9691162109375,-5.996534824371338,147.21833801269554,-3.9902749061584326]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Manus" bounds={[142.8240509033203,-2.9321799278259277,148.20832824707034,-0.7558330297469982]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Milne Bay" bounds={[148.96670532226562,-11.655380249023438,154.39971923828136,-8.30111217498781]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Morobe" bounds={[145.74201965332054,-8.032334327697754,148.1841735839846,-5.280276775360104]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="New Ireland" bounds={[149.50520324707054,-4.851665019989014,154.75170898437503,-1.314795970916769]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="North Solomons" bounds={[154.09963989257824,-6.880917072296143,157.03778076171884,-4.355535984039303]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Northern" bounds={[147.004119873047,-9.97723388671875,149.44386291503915,-8.004035949706989]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Sandaun" bounds={[140.9997100830078,-5.378980159759408,143.09657287597693,-2.602708101272579]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Southern Highlands" bounds={[142.06697082519554,-6.8236351013183025,144.6881103515626,-4.941134929656986]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="West New Britain" bounds={[148.3077850341798,-6.325832843780461,151.69001770019528,-4.535143852233887]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Western" bounds={[140.8343505859375,-9.524073600769043,143.92416381835966,-4.987235069274884]} drillProvince={this.drillProvince.bind(this)}/>
+                    <ProvinceListItem primaryText="Western Highlands" bounds={[143.7709197998047,-6.389835834503117,145.02641296386744,-5.218733787536607]} drillProvince={this.drillProvince.bind(this)}/>
+                  </List>
                 </React.Fragment>
                 : null}
           </Tab>
           <Tab 
-            label="Provinces"
-            value="provinces"
+            label="Province"
+            value="province"
             >
+              {this.state.selectedProvince ? 
+                <React.Fragment>
+                  <List>
+                    <ListItem primaryText={this.state.selectedProvince.props.primaryText} />
+                  </List> 
+                </React.Fragment>
+                : null}
           </Tab>
         </Tabs>
       </React.Fragment>;
@@ -209,9 +251,11 @@ class IndicatorChart extends React.Component {
             titleStyle={{fontSize:'17px'}} 
             children={children}
           />
+          {this.state.value == "summary" ?
           <CardText 
             style={{padding:'0px 16px 16px 16px',fontSize:'13px'}}>{this.props.indicator.indicatorListItem.props.desc ? this.props.indicator.indicatorListItem.props.desc : "No description."}
           </CardText>
+          : null}
         </Card> : null
     );
   }
@@ -545,6 +589,29 @@ class HorizontalLinearStepper extends React.Component {
   }
 }
 
+function addGlobalForestWatch(map) {
+    map.addLayer({
+        'id': 'GlobalForestWatch',
+        'type': 'raster',
+        'source': {
+            'type': 'raster',
+            // "attribution":"Potapov P. et al. 2008. Mapping the World's Intact Forest Landscapes by Remote Sensing. Ecology and Society, 13 (2)",
+            'tiles': [
+                // 'https://globalforestwatch-624153201.us-west-1.elb.amazonaws.com/arcgis/services/ForestCover_lossyear/ImageServer/WMSServer?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&width=256&height=256&layers=0'
+                // 'https://50.18.182.188:6080/arcgis/services/ForestCover_lossyear/ImageServer/WMSServer?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&width=256&height=256&layers=0'
+                'http://gis-treecover.wri.org/arcgis/services/ForestCover_lossyear/ImageServer/WMSServer?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&width=256&height=256&layers=0'
+            ],
+            'tileSize': 256
+        },
+        "layout": {
+            "visibility": "visible"
+        },
+        'maxzoom': 14,
+        'minzoom': 7,
+        'paint': {}
+    }, 'Intact Forest 2013');
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -552,6 +619,7 @@ class App extends Component {
   }
   mapLoaded(e) {
     this.setState({ map: e.target });
+    addGlobalForestWatch(e.target);
   }
   render() {
     return (
