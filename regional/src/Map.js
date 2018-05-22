214e0d0594bd460d9bfe4125513b44942f74d9ed
Andrew Cottam <a.cottam@gmail.com>
2018-05-22 19:10:32 +0200

    trying to sort issues on the new machine

--- regional/src/Map.js
+++ regional/src/Map.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import ReactMapboxGl from "react-mapbox-gl";
 import MapPopup from './MapPopup.js';
-// import INITIAL_STYLE from "./cbd11.json";
+import INITIAL_STYLE from "./cbd11.json";
 
 const ReactMap = ReactMapboxGl({
   accessToken: "pk.eyJ1IjoiYmxpc2h0ZW4iLCJhIjoiMEZrNzFqRSJ9.0QBRA2HxTb8YHErUFRMPZg",
@@ -11,7 +11,7 @@ const ReactMap = ReactMapboxGl({
 
 const INITIAL_CENTER = [20, -2];
 const INITIAL_ZOOM = [6];
-const INITIAL_STYLE = "mapbox://styles/blishten/cj6f4n2j026qf2rnunkauikjm";
+// const INITIAL_STYLE = "mapbox://styles/blishten/cj6f4n2j026qf2rnunkauikjm";
 const CONTAINER_STYLE = { height: "100vh", width: "100vw" };
 
 class Map extends React.Component {
